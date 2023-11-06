3631c162fe3a0cec98b9514414a0eb4a3afdfce9
Igor <igorponticelli8@gmail.com>
2023-11-06 12:57:07 -0300

    FisrtInterfaceCommit

---
+++ Ferroviaria/src/main/java/org/example/Interface.java
@@ -0,0 +1,11 @@
+package org.example;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+// ver quais imports usar
+
+
+public class Interface {
+    
+}
